17b719479b114e87bbf1d11574ac29260037e265
Wagner Fonseca <wagner.fonseca@luizalabs.com>
2018-11-16 17:51:55 -0200

    Fix

--- 06.install-intellijIDEA.sh
+++ 06.install-intellijIDEA.sh
@@ -11,15 +11,4 @@ download_resource $URL
 unpack $DESTINY_PATH
 create_symlink "bin/idea.sh" "idea.sh"
 
-cat > ~/.local/share/applications/intellijidea.desktop <<EOL
-[Desktop Entry]
-Encoding=UTF-8
-Name=Intellij IDEA
-Exec=intellijIDEA
-Icon=/opt/Intellij-IDEA-Community/ideaIU-2018.2.6/bin/idea.png
-Terminal=false
-Type=Application
-Categories=Development;
-EOL
-
 remove_source
