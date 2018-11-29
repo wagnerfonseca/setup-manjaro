6ee50cd73e90e63eca296c3c664916f8c098e590
Wagner Fonseca <wagner.fonseca@luizalabs.com>
2018-11-29 14:55:39 -0200

    Configurações para alt+tab

--- 02.setup-manjaro-xfce.sh
+++ 02.setup-manjaro-xfce.sh
@@ -1,3 +1,14 @@
-#!/bin/bash
+#! /bin/sh
+# A short script to toggle compositing on or off for XFCE Window Manager (XFWM)
+#
+# @author   Wagner fonseca 
+# @created  2018-11-29
 
+## DPI
 xfconf-query -c xsettings -p /Xft/DPI -s 114
+
+
+if $(xfconf-query --channel=xfwm4 --property=/general/cycle_preview) == "true"
+then
+	xfconf-query --channel=xfwm4 --property=/general/cycle_preview --set=false
+fi
