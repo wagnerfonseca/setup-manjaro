545a173caef7385d4aac82b0108e2c5ae1c80817
Wagner Fonseca <wagner.fonseca@luizalabs.com>
2019-03-12 10:58:59 -0300

    Pequeno ajuste

--- 01.install-utilities.sh
+++ 01.install-utilities.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-. script_install.sh
+. basic_pac_install.sh
 
 #package=git;
 #_install "${package}";
 echo "Serão instalados diversos pacotes..."
 
-_installMany "youtube-dl" "curl" "vim" "tree" "htop" "tilix" "yaourt" "figlet" "cowsay" "screenfetch" "xclip" "okular" "jq" "pulseaudio-equalizer-ladspa" "pulseeffects"
+_installMany "youtube-dl" "curl" "vim" "tree" "htop" "tilix" "yay" "figlet" "cowsay" "screenfetch" "xclip" "okular" "jq" "pulseaudio-equalizer-ladspa" "pulseeffects" 
 
-# sudo yaourt -S slack-desktop google-chrome franz-bin megasync visual-studio-code-bin albert
+# sudo yay -S slack-desktop google-chrome franz-bin megasync visual-studio-code-bin albert
