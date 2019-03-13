50efd4bff180ee18791bbf74ecffa43ab0a97eb0
Wagner Fonseca <wagner.fonseca@luizalabs.com>
2019-03-13 08:45:32 -0300

    Mais pacotes

--- 01.install-utilities.sh
+++ 01.install-utilities.sh
@@ -5,6 +5,6 @@
 #_install "${package}";
 echo "Serão instalados diversos pacotes..."
 
-_installMany "youtube-dl" "curl" "vim" "tree" "htop" "tilix" "yay" "figlet" "cowsay" "screenfetch" "xclip" "okular" "jq" "pulseaudio-equalizer-ladspa" "pulseeffects" 
+_installMany "youtube-dl" "curl" "vim" "tree" "htop" "tilix" "yay" "figlet" "cowsay" "screenfetch" "xclip" "jq" "pulseaudio-equalizer-ladspa" "pulseeffects" "synapse" "slack-desktop" "google-chrome" "franz-bin" "megasync" "visual-studio-code-bin" "bluez-alsa-git"
 
-# sudo yay -S slack-desktop google-chrome franz-bin megasync visual-studio-code-bin albert
+# sudo yay -S slack-desktop google-chrome franz-bin megasync visual-studio-code-bin bluez-alsa-git
